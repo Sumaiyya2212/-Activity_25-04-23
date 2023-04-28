fbd8f30ccafe371ab9b3fd0b25113dfaa2f5b274
Sumaiyya2212 <98161123+Sumaiyya2212@users.noreply.github.com>
2023-04-28 18:17:05 +0530

    Update FileWriteDemo.java

--- FileWriteDemo.java
+++ FileWriteDemo.java
@@ -1,1 +1,35 @@
+package filestream;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class FileWriteDemo {
+
+	public static void main(String[] args) {
+		//write in a file
+		try {
+			FileWriter fw = new FileWriter(" ");
+			fw.write("We are learning to use File writer.");
+			fw.close();
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		//read from a file
+		try {
+	FileReader fr = new FileReader("E:\\Java Text Files\\Letter.txt");
+		    int i;
+		    while((i=fr.read())!=-1)
+		    {
+		    	System.out.print((char)i);
+		    }
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		System.out.println();
+		System.out.println("Sucess");
+	}
+
+}
